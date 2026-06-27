3fc66fbbe544a89c7d7ea946cd5d85ac914b3e57
danielavallecillo <daniela2003vf@gmail.com>
2026-06-26 21:47:42 -0600

    en el Agregar puse para que se pudieran poner JAJJAJ cada coso pero personalizado :D

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,16 +29,24 @@
         private void InitializeComponent()
         {
             splitContainer1 = new SplitContainer();
+            numericUpDown1 = new NumericUpDown();
+            button2 = new Button();
             button3 = new Button();
-            domainUpDown1 = new DomainUpDown();
             textBox1 = new TextBox();
             dataGridView1 = new DataGridView();
-            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            numericUpDown2 = new NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             SuspendLayout();
             // 
             // splitContainer1
@@ -49,21 +57,44 @@
             // 
             // splitContainer1.Panel1
             // 
+            splitContainer1.Panel1.Controls.Add(numericUpDown2);
+            splitContainer1.Panel1.Controls.Add(label4);
+            splitContainer1.Panel1.Controls.Add(textBox2);
+            splitContainer1.Panel1.Controls.Add(label3);
+            splitContainer1.Panel1.Controls.Add(label2);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(numericUpDown1);
             splitContainer1.Panel1.Controls.Add(button2);
             splitContainer1.Panel1.Controls.Add(button3);
-            splitContainer1.Panel1.Controls.Add(domainUpDown1);
             splitContainer1.Panel1.Controls.Add(textBox1);
             // 
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(dataGridView1);
-            splitContainer1.Size = new Size(800, 450);
-            splitContainer1.SplitterDistance = 386;
+            splitContainer1.Size = new Size(1134, 512);
+            splitContainer1.SplitterDistance = 547;
             splitContainer1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(12, 156);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(162, 27);
+            numericUpDown1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(365, 114);
+            button2.Name = "button2";
+            button2.Size = new Size(162, 39);
+            button2.TabIndex = 5;
+            button2.Text = "Agregar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(209, 30);
+            button3.Location = new Point(365, 39);
             button3.Name = "button3";
             button3.Size = new Size(162, 39);
             button3.TabIndex = 4;
@@ -71,17 +102,9 @@
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
-            // domainUpDown1
-            // 
-            domainUpDown1.Location = new Point(12, 126);
-            domainUpDown1.Name = "domainUpDown1";
-            domainUpDown1.Size = new Size(162, 27);
-            domainUpDown1.TabIndex = 3;
-            domainUpDown1.Text = "domainUpDown1";
-            // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 42);
+            textBox1.Location = new Point(12, 69);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(162, 27);
             textBox1.TabIndex = 2;
@@ -93,23 +116,64 @@
             dataGridView1.Location = new Point(0, 0);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(410, 450);
+            dataGridView1.Size = new Size(583, 512);
             dataGridView1.TabIndex = 0;
             // 
-            // button2
+            // label1
             // 
-            button2.Location = new Point(209, 114);
-            button2.Name = "button2";
-            button2.Size = new Size(162, 39);
-            button2.TabIndex = 5;
-            button2.Text = "Agregar";
-            button2.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 133);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Edad:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 238);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Puesto:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 270);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(162, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 347);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 20);
+            label4.TabIndex = 11;
+            label4.Text = "Sueldo:";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(12, 370);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(162, 27);
+            numericUpDown2.TabIndex = 12;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1134, 512);
             Controls.Add(splitContainer1);
             Name = "Form1";
             Text = "Form1";
@@ -118,7 +182,9 @@
             splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ResumeLayout(false);
         }
 
@@ -126,9 +192,15 @@
 
         private SplitContainer splitContainer1;
         private DataGridView dataGridView1;
-        private DomainUpDown domainUpDown1;
         private TextBox textBox1;
         private Button button3;
         private Button button2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,31 @@ namespace Tarea_3_Paradigmas
 
             }
         }
+
+        //BOTON AGREGAR
+        private void button2_Click(object sender, EventArgs e)
+        {
+            E.M_EMPLEADO emple = new E.M_EMPLEADO();
+            emple.nombre = this.textBox1.Text;
+            emple.puesto = this.textBox2.Text;
+            emple.edad = Convert.ToInt32(this.numericUpDown1.Value);
+            emple.sueldo = Convert.ToDouble(this.numericUpDown2.Value); 
+
+            string cadena = "workstation id=empresa2.mssql.somee.com;packet size=4096;user id=danielavf_SQLLogin_1;pwd=DaniEmpresa2026;data source=empresa2.mssql.somee.com;persist security info=False;initial catalog=empresa2;TrustServerCertificate=True";
+            string sql = @"insert into
+                EMPLEADOS(NOMBRE, EDAD, PUESTO, SUELDO)
+                VALUES(@NOMBRE, @EDAD, @PUESTO, @SUELDO)";
+            string sql2 = "select * from EMPLEADOS";
+
+
+            using (var con = new Microsoft.Data.SqlClient.SqlConnection(cadena))
+            {
+                con.Open();
+                con.Execute(sql, emple);
+                this.dataGridView1.DataSource = con.Query<E.M_EMPLEADO>(sql2).ToList();
+                con.Close();
+            }
+        }
     }
 
 }
